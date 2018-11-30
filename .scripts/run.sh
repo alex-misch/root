f2fed6f0ffc95d6dcbb858b12c6b0c1ba39a9cb3
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-11-30 18:50:58 +0300

    refactor run script

--- .scripts/run.sh
+++ .scripts/run.sh
@@ -7,8 +7,8 @@ set -eux
 NODE='ci'
 
 # install external dependencies on docker image
-apt-get update
-apt-get install -y jq
+# apt-get update
+# apt-get install -y jq
 # apk add --update --no-cache \
 # 	jq \
 	# git \
@@ -28,19 +28,21 @@ apt-get install -y jq
 # this is cli that will be invoked by `base` (for example)
 .scripts/fmt.sh ${NODE}
 .scripts/build.sh ${NODE}
-# copy bin from /go/bin to our dir (conf needs it here)
+
+# copy necessary bins from /go/bin to our dir (conf needs it here because related path to node root)
 cp /go/bin/${NODE}-$(uname -s)-$(uname -m) ./${NODE}
 
-
-# set application variables for run base
 # TODO move to special config in future named boomfunc.yaml
 # TODO https://github.com/urfave/cli#values-from-alternate-input-sources-yaml-toml-and-others
-export BMP_DEBUG_MODE=true
-export BMP_CONFIG='./ci/router.yml'
+# export BMP_DEBUG_MODE=true
+# export BMP_CONFIG='./ci/router.yml'
 # export BMP_CONFIG='https://bitbucket.org/!api/2.0/snippets/letsnetdevinternal/eazARA/files/example.yml'
-export BMP_APPLICATION_LAYER='http'
-export BMP_DEBUG_MODE=true
+# export BMP_APPLICATION_LAYER='http'
 # export BMP_WORKER_NUM=8
 
-# run base with cli onboard
-/go/bin/base-$(uname -s)-$(uname -m) run tcp
+# run base with microservice cli onboard
+# set application variables for run base
+BMP_DEBUG_MODE=true \
+BMP_CONFIG='./ci/router.yml' \
+BMP_APPLICATION_LAYER='http' \
+	/go/bin/base-$(uname -s)-$(uname -m) run tcp
