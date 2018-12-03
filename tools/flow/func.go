8427ed58f8595322215dc6adae322c7eeb80fcd9
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-12-03 17:13:56 +0300

    add tests to func type

---
+++ tools/flow/func.go
@@ -0,0 +1,14 @@
+package flow
+
+import (
+	"context"
+)
+
+// Func is basic type of step (executable)
+// just wrapper for function that implements Step interface
+type Func func(context.Context) error
+
+// Run implements Step interface
+func (f Func) Run(ctx context.Context) error {
+	return f(ctx)
+}
