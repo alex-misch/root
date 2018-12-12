0e4ce2ad583e6cd4df777981cd51c2c59ac22271
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-12-12 17:16:59 +0300

    #14 fix tests

--- base/tools/poller/common_test.go
+++ base/tools/poller/common_test.go
@@ -47,37 +47,88 @@ func TestPollerPublic(t *testing.T) {
 	})
 
 	t.Run("Events", func(t *testing.T) {
-		poller.Add(r.Fd())
-		poller.Add(w.Fd())
+		t.Run("read", func(t *testing.T) {
+			if err := poller.Add(r.Fd()); err != nil {
+				t.Fatal(err)
+			}
 
-		re, ce, err := poller.Events()
-		if err != nil {
-			t.Fatal(err)
-		}
-		// now only w part is ready for writing -> len == 1
-		if len(re) != 0 {
-			t.Fatal("Unexpected number of read events, expected 0")
-		}
-		if len(ce) != 0 {
-			t.Fatal("Unexpected number of closed events, expected 0")
-		}
+			// now it is nothing to read - .Events() will block function
+			// write imitation
+			fmt.Fprint(w, "some playload")
 
-		// write imitation
-		fmt.Fprint(w, "some playload")
+			re, ce, err := poller.Events()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// now w and r parts is ready -> len == 2
+			if len(re) != 1 {
+				t.Fatal("Unexpected number of read events, expected 1")
+			}
+			if len(ce) != 0 {
+				t.Fatal("Unexpected number of closed events, expected 0")
+			}
+			if re[0].Fd() != r.Fd() {
+				t.Fatal("Unexpected event fd")
+			}
+		})
 
-		re, ce, err = poller.Events()
-		if err != nil {
-			t.Fatal(err)
-		}
-		// now w and r parts is ready -> len == 2
-		if len(re) != 1 {
-			t.Fatal("Unexpected number of read events, expected 1")
-		}
-		if len(ce) != 0 {
-			t.Fatal("Unexpected number of closed events, expected 0")
-		}
-		if re[0].Fd() != r.Fd() {
-			t.Fatal("Unexpected event fd")
-		}
+		t.Run("close", func(t *testing.T) {
+			r, w, _ := os.Pipe()
+
+			if err := poller.Add(r.Fd()); err != nil {
+				t.Fatal(err)
+			}
+
+			// now it is nothing to read - .Events() will block function
+			// close imitation
+			if err := w.Close(); err != nil {
+				t.Fatal(err)
+			}
+
+			re, ce, err := poller.Events()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// now w and r parts is ready -> len == 2
+			if len(re) != 0 {
+				t.Fatal("Unexpected number of read events, expected 0")
+			}
+			if len(ce) != 1 {
+				t.Fatal("Unexpected number of closed events, expected 1")
+			}
+			if ce[0].Fd() != r.Fd() {
+				t.Fatal("Unexpected event fd")
+			}
+		})
+
+		t.Run("mixed", func(t *testing.T) {
+			// case when we write to pipe and than closes it - only one event must be fetched (closing)
+			r, w, _ := os.Pipe()
+
+			if err := poller.Add(r.Fd()); err != nil {
+				t.Fatal(err)
+			}
+			if err := poller.Add(w.Fd()); err != nil {
+				t.Fatal(err)
+			}
+
+			fmt.Fprint(w, "some playload") //write imitation
+			w.Close()                      // close imitation
+
+			re, ce, err := poller.Events()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// now w and r parts is ready -> len == 2
+			if len(re) != 0 {
+				t.Fatal("Unexpected number of read events, expected 0")
+			}
+			if len(ce) != 1 {
+				t.Fatal("Unexpected number of closed events, expected 1")
+			}
+			if ce[0].Fd() != r.Fd() {
+				t.Fatal("Unexpected event fd")
+			}
+		})
 	})
 }
--- base/tools/poller/epoll_test.go
+++ base/tools/poller/epoll_test.go
@@ -27,37 +27,25 @@ func TestPollerPrivate(t *testing.T) {
 	})
 
 	t.Run("wait", func(t *testing.T) {
-		r, w, _ := os.Pipe()
-		epoll.Add(r.Fd())
-		epoll.Add(w.Fd())
+		r1, w1, _ := os.Pipe()
+		r2, w2, _ := os.Pipe()
+		epoll.Add(r1.Fd())
+		epoll.Add(w1.Fd())
+		epoll.Add(r2.Fd())
+		epoll.Add(w2.Fd())
+
+		// now nothing available
+		// first pipe - write, second pipe - close
+		fmt.Fprint(w1, "some playload")
+		w2.Close()
 
 		events, err := epoll.wait()
 		if err != nil {
 			t.Fatal(err)
 		}
-		// now only w part is ready for writing -> len == 1
-		if len(events) != 1 {
-			t.Fatal("Unexpected number of events, expected 1")
-		}
-		if events[0].Fd != int32(w.Fd()) {
-			t.Fatal("Unexpected event fd")
-		}
 
-		fmt.Fprint(w, "some playload")
-
-		events, err = epoll.wait()
-		if err != nil {
-			t.Fatal(err)
-		}
-		// now w and r parts is ready -> len == 2
 		if len(events) != 2 {
 			t.Fatal("Unexpected number of events, expected 2")
-		}
-		if events[0].Fd != int32(w.Fd()) {
-			t.Fatal("Unexpected event fd")
-		}
-		if events[1].Fd != int32(r.Fd()) {
-			t.Fatal("Unexpected event fd")
 		}
 	})
 }
