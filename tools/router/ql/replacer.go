fcaa85477fb8159e5f0d3e096a6a938db1256099
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-12-27 20:15:36 +0300

    add get query replacer
    still not working with negative param
    !b means not b get param

--- tools/router/ql/replacer.go
+++ tools/router/ql/replacer.go
@@ -5,6 +5,16 @@ import (
 	"strings"
 )
 
+var (
+	// regex wrapper for get param with concrete value, where 1 - param name, 2 - param value
+	ValueWrapper = "(?:^|&)%[1]s=(?P<%[1]s>%[2]s)(?:$|&)"
+	// regex wrapper for get param without any required value, where 1 - param name
+	UnvalueWrapper = "(?:^|&)%[1]s(?:=(?P<%[1]s>[^&#]*))?"
+	// regex wrapper for binding part's regex to the general expression
+	JoinWrapper = "(?=.*%s)"
+)
+
+// Replacer is function that returns reg expression as string from source
 type Replacer func(string) string
 
 // defaultReplacer replacer replaces with non capturing group
@@ -27,11 +37,37 @@ func groupReplacer(group string) Replacer {
 // queryReplacer replacer replaces according to the logic of parsing GET parameters
 // main idea and key concepts:
 // there are some cases and states for get params:
-// 1) exists {?:a,b,c}
-// 2) exists and equal {?:a=foo,b,c=bar}
-// 3) not exists {?:a=foo,!b,c=bar}
-// 4) combine multiple (separate by ',')
+// 1) exists {?:a&b&c}
+// 2) exists and equal {?:a=foo&b&c=bar}
+// 3) not exists {?:a=foo&!b&c=bar}
+// 4) combine multiple (separate by '&')
+// TODO: for now it is only AND condition -> how we can solve 'a OR b' in query?
 var queryReplacer Replacer = func(source string) string {
-	// Phase 1. Split params by special `ql` separator
-	return ""
+	// Prephase. check for empty source -> return fallback
+	if strings.Trim(source, " ") == "" {
+		return "FALLBACK!!"
+	}
+
+	// Phase 1. Split params by special `ql` separator - & (like in query)
+	parts := strings.Split(source, "&")
+	reparts := make([]string, len(parts))
+
+	for i, part := range parts {
+		// Phase 2, get part's regex based on concrete value or not
+		// split by `=`
+		var regex string
+
+		if items := strings.SplitN(part, "=", 2); len(items) == 2 && items[1] != "" {
+			// case when value exists
+			regex = fmt.Sprintf(ValueWrapper, strings.Trim(items[0], " "), items[1])
+		} else {
+			// case when value undefined - all is good
+			regex = fmt.Sprintf(UnvalueWrapper, strings.Trim(items[0], " "))
+		}
+
+		// Phase 3. Join part's regex to common big regex
+		reparts[i] = fmt.Sprintf(JoinWrapper, regex)
+	}
+
+	return fmt.Sprintf("^%s.*$", strings.Join(reparts, ""))
 }
--- tools/router/ql/replacer_test.go
+++ tools/router/ql/replacer_test.go
@@ -33,11 +33,40 @@ func TestReplacer(t *testing.T) {
 			out    string // expected value of replacer output
 		}{
 			{"", "(?<lolkek>)"},
+			{" ", "(?<lolkek> )"},
 			{"foo|bar|baz", "(?<lolkek>foo|bar|baz)"},
 			{"foo|bar|*", "(?<lolkek>foo|bar|.*)"},
 		}
 
 		var replacer Replacer = groupReplacer("lolkek")
+
+		for i, tt := range tableTests {
+			t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+				if out := replacer(tt.source); out != tt.out {
+					t.Fatalf("Expected %q, got %q", tt.out, out)
+				}
+			})
+		}
+	})
+
+	t.Run("query", func(t *testing.T) {
+		tableTests := []struct {
+			source string // source
+			out    string // expected value of replacer output
+		}{
+			{"", "TODO"},  // fallback value
+			{" ", "TODO"}, // fallback value
+			// unvalued
+			{"foobar", "^(?=.*(?:^|&)foobar(?:=(?P<foobar>[^&#]*))?).*$"},
+			{" foobar  ", "^(?=.*(?:^|&)foobar(?:=(?P<foobar>[^&#]*))?).*$"}, // key is always trimmed
+			// valued
+			{"foobar=lolkek", "^(?=.*(?:^|&)foobar=(?P<foobar>lolkek)(?:$|&)).*$"},
+			{" foobar  =lolkek", "^(?=.*(?:^|&)foobar=(?P<foobar>lolkek)(?:$|&)).*$"}, // key is always trimmed
+			// mixed
+			{"a&b=& c =foobar& d  =foobar==", "^(?=.*(?:^|&)a(?:=(?P<a>[^&#]*))?)(?=.*(?:^|&)b(?:=(?P<b>[^&#]*))?)(?=.*(?:^|&)c=(?P<c>foobar)(?:$|&))(?=.*(?:^|&)d=(?P<d>foobar==)(?:$|&)).*$"}, // a, b - anything, c, d - by value
+		}
+
+		var replacer Replacer = queryReplacer
 
 		for i, tt := range tableTests {
 			t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
