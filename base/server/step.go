ea2746908db44708770f09322d5545b974a356ac
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-01-09 17:37:16 +0300

    step prototype

--- base/server/step.go
+++ base/server/step.go
@@ -1,56 +1,74 @@
 package server
 
-import (
-	"context"
-
-	"github.com/boomfunc/root/tools/flow"
-)
-
-var view = flow.Dispatcher(
-	2, // os.cpu for example
-	flow.Group(
-
-		// NOTE: this block is about parsing incoming stream
-		// ErrBadRequest or ErrRouteNotFound might be thrown
-		flow.Func(func(ctx context.Context) error {
-			// app unpack request
-			// NOTE: app fetch from context - otherwise orphan
-			// NOTE: write request to context
-			// TODO: return app.packer.Unpack(fl.Ctx, fl.RWC)
-			return nil
-		}),
-		flow.Func(func(ctx context.Context) error {
-			// router match
-			// TODO: separate router and application
-			// NOTE: router fetch from context - otherwise orphan
-			// TODO: router.Match(req.Url)
-			// TODO: save pipeline to ctx
-			return nil
-		}),
-
-		// NOTE: this block is about pipeline logic and concrete view per route
-		// ErrServerError only might be thrown
-		flow.Concurrent( // NOTE: concurrent with waiting (one part reads and process, another write answer)
-			flow.Func(func(ctx context.Context) error {
-				// pipeline run
-				// NOTE: fetch pipeline, input and output from context - otherwise orphan
-				// TODO: return pipeline.Run(ctx, req.Input, pw)
-				return nil
-			}),
-			flow.Func(func(ctx context.Context) error {
-				// app pack request
-				// NOTE: fetch input and output from context - otherwise orphan
-				// NOTE: app fetch from context - otherwise orphan
-				// TODO: return app.packer.Pack(pr, fl.RWC)
-				return nil
-			}),
-		),
-
-		// NOTE: this block is about debugging and logging
-		// NOTE this step might be omitted if error (but it's no good)
-		flow.Func(func(ctx context.Context) error {
-			// access log and error log
-			return nil
-		}),
-	),
-)
+// import (
+// 	"context"
+//
+// 	"github.com/boomfunc/root/tools/router"
+// 	"github.com/boomfunc/root/tools/flow"
+// )
+//
+// var view flow.Step = flow.Dispatcher(
+// 	2, // os.cpu for example
+// 	flow.Group(
+//
+// 		// NOTE: this block is about parsing incoming stream
+// 		// ErrBadRequest or ErrRouteNotFound might be thrown
+// 		flow.Func(func(ctx context.Context) error {
+// 			unpacker, ok := ctx.Value("base.unpacker").(application.Unpacker)
+// 			if !ok {
+// 				return flow.ErrStepOrphan
+// 			}
+//
+// 			return unpacker.Unpack(ctx)
+// 		}),
+// 		flow.Func(func(ctx context.Context) error {
+// 			// fetch url
+// 			url, ok := ctx.Value("base.request.url").(string)
+// 			if !ok {
+// 				return flow.ErrStepOrphan
+// 			}
+//
+// 			// fetch router from context - otherwise orphan
+// 			router, ok := ctx.Value("base.router").(router.Router)
+// 			if !ok {
+// 				return flow.ErrStepOrphan
+// 			}
+//
+// 			// router match
+// 			route, err := router.Match(url)
+// 			if err != nil {
+// 				return err
+// 			}
+//
+// 			// save pipeline to ctx (as view)
+// 			// TODO: save params to ctx
+// 			ctx = context.WithValue()
+// 			return nil
+// 		}),
+//
+// 		// NOTE: this block is about pipeline logic and concrete view per route
+// 		// ErrServerError only might be thrown
+// 		flow.Concurrent( // NOTE: concurrent with waiting (one part reads and process, another write answer)
+// 			flow.Func(func(ctx context.Context) error {
+// 				// pipeline run
+// 				// NOTE: fetch pipeline, input and output from context - otherwise orphan
+// 				// TODO: return pipeline.Run(ctx, req.Input, pw)
+// 				return nil
+// 			}),
+// 			flow.Func(func(ctx context.Context) error {
+// 				// app pack request
+// 				// NOTE: fetch input and output from context - otherwise orphan
+// 				// NOTE: app fetch from context - otherwise orphan
+// 				// TODO: return app.packer.Pack(pr, fl.RWC)
+// 				return nil
+// 			}),
+// 		),
+//
+// 		// NOTE: this block is about debugging and logging
+// 		// NOTE this step might be omitted if error (but it's no good)
+// 		flow.Func(func(ctx context.Context) error {
+// 			// access log and error log
+// 			return nil
+// 		}),
+// 	),
+// )
