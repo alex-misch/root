8b76724a09ce120fb1c0954b7bb7054bf44e3538
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-01-24 18:30:53 +0300

    fix Wait feature using sync.Cond
    NOT TESTED

--- tools/kvs/namespace.go
+++ tools/kvs/namespace.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Namespace struct {
-	mutex   sync.Mutex                  // this mutex guards variables below
-	pending map[string]chan interface{} // pending is keys that somebody is waiting for
-	dirty   map[string]interface{}      // inner store
+	mutex   sync.Mutex             // this mutex guards variables below
+	pending map[string]*sync.Cond  // pending is keys that somebody is waiting for
+	dirty   map[string]interface{} // inner store
 }
 
 // NewNamespace initialize and returns new key-value separate database's namespace
 func NewNamespace() *Namespace {
 	return &Namespace{
-		pending: make(map[string]chan interface{}, 0),
+		pending: make(map[string]*sync.Cond, 0),
 		dirty:   make(map[string]interface{}, 0),
 	}
 }
@@ -27,6 +27,13 @@ func (ns *Namespace) Get(key string) interface{} {
 	return ns.dirty[key]
 }
 
+// set stores value by key
+// private api
+// not thread safety
+func (ns *Namespace) set(key string, value interface{}) {
+	ns.dirty[key] = value
+}
+
 // Set stores value by key
 // if somebody wait for value - send signal and
 func (ns *Namespace) Set(key string, value interface{}) {
@@ -34,36 +41,35 @@ func (ns *Namespace) Set(key string, value interface{}) {
 	defer ns.mutex.Unlock()
 
 	// set value
-	ns.dirty[key] = value
+	ns.set(key, value)
 
 	// also check for pending
-	if ch, ok := ns.pending[key]; ok {
-		select {
-		case ch <- value:
-		default: // for non blocking - nobody waits - no need
-		}
-
-		// grabage
-		close(ch)
+	if cond, ok := ns.pending[key]; ok && cond != nil {
+		cond.Broadcast()
+		// garbage
 		delete(ns.pending, key)
 	}
 }
 
 // Wait is for waiting nearest .Set() invokes for provided key
-// TODO: look better for a sync.Cond, because we need some broadcast multiple routines
-// TODO: current implementation too raw
 func (ns *Namespace) Wait(key string) {
 	ns.mutex.Lock()
-	defer ns.mutex.Unlock()
 
-	if _, ok := ns.dirty[key]; ok {
-		// value exists, no need to wait
-		return
-	} else {
-		// add to pending and create channel
-		ch := make(chan interface{})
-		ns.pending[key] = ch
+	cond := ns.pending[key]
+
+	if cond == nil { // check for existsing waiting condition in pending
+		// condition not exists, or no wait invokes before - create new cond
+		cond = sync.NewCond(&sync.Mutex{})
+		ns.pending[key] = cond // save cond to future use
 	}
+
+	ns.mutex.Unlock()
+
+	// freeze thread for waiting broadcast
+	// NOTE: namespace mutex unlocked, so we don't lock for another operations
+	cond.L.Lock()
+	cond.Wait()
+	cond.L.Unlock()
 }
 
 // delete clears value stored in key
