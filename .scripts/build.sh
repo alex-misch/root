e34e7a520372799904ea29ef3af7a73190fab51a
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-01-24 20:35:35 +0300

    tmp builder

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,6 +3,7 @@ set -eux
 
 # if first arg is node in monorepo graph - apply commands to this node (package),
 # otherwise - to root (all repo nodes)
-NODE=${1:-.}
+# NODE=${1:-.}
+NODE='ssr-bmpjs'
 
 .scripts/build-plugin.sh ${NODE}
