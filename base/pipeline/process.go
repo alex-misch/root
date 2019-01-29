f19f3de45971657232e06710d278df7144cb3d79
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-01-29 14:15:29 +0300

    base/pipeline: additional logging

--- base/pipeline/process.go
+++ base/pipeline/process.go
@@ -27,38 +27,13 @@ func (p *process) copy() Layer {
 }
 
 func (p *process) prepare(ctx context.Context) error {
-	// p.cmd.Stdin = p.stdin
-	// p.cmd.Stdout = p.stdout
-	//
-	// if p.cmd.Process == nil {
-	// 	if err := p.cmd.Start(); err != nil {
-	// 		return err
-	// 	}
-	// }
-
 	return nil
 }
 
 // check method guarantees that the object can be launched at any time
 // process is piped
 func (p *process) check(ctx context.Context) error {
-	// check layer piped
-	if err := p.checkStdio(); err != nil {
-		return err
-	}
-
-	// // check command is valid and ready
-	// if p.cmd == nil {
-	// 	return errors.New("pipeline: Process without underlying exec.Cmd")
-	// }
-
-	// check cmd stdio
-	// if p.cmd.Stdin == nil || p.cmd.Stdout == nil {
-	// 	return errors.New("pipeline: Process's underlying exec.Cmd not piped")
-	// }
-
-	// process ready for run
-	return nil
+	return p.checkStdio()
 }
 
 // NOTE: this kind also hungs - problem not here!!!!
@@ -81,12 +56,14 @@ func (p *process) run(ctx context.Context) error {
 
 	if cmd.Process == nil {
 		if err := cmd.Start(); err != nil {
-			return err
+			return fmt.Errorf("base/pipeline/process: %s", err)
 		}
 	}
 
+	// exit status resolves here
+	// additionally fetch some information from stderr
 	if err := cmd.Wait(); err != nil {
-		return err
+		return fmt.Errorf("base/pipeline/process: %s\n%s", err, stderr)
 	}
 
 	// check for case exit code = 0, but stderr got something
@@ -98,19 +75,5 @@ func (p *process) run(ctx context.Context) error {
 }
 
 func (p *process) close(ctx context.Context) error {
-	// defer func() {
-	// 	p.cmd = nil
-	// }()
-
 	return p.closeStdio()
-
-	// defer func() {
-	// 	if err != nil {
-	// 		p.closeStdio()
-	// 	} else {
-	// 		err = p.closeStdio()
-	// 	}
-	// }()
-	//
-	// return
 }
