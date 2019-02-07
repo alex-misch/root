2e2c170688e3ef13d9a3228250355861015bc590
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-02-07 15:59:58 +0300

    fix build bin script (beginning of cross compiling)

--- .scripts/build-bin.sh
+++ .scripts/build-bin.sh
@@ -28,29 +28,53 @@ VERSION="${CIRCLE_TAG:=LOCAL}"
 
 # calculate build/compile specific variables
 ldflags="-X 'main.VERSION=${VERSION}' -X 'main.TIMESTAMP=${TIMESTAMP}'"
+### COMPILE
 
-# compile and build
-# linux 64 bit
-# - alpine, ubuntu
-CGO_ENABLED=0 \
+# linux 64 bit (alpine, ubuntu)
+# NOTE: working
+CGO_ENABLED=1 \
+CC='gcc' \
 GOOS=linux GOARCH=amd64 go build \
-	-a -tags netgo \
+	`# -a # force rebuilding of packages that are already up-to-date` \
+	`# -tags netgo # make sure we use built-in net package and not the system’s one` \
 	-v \
-	-ldflags '-w -extldflags "-static"' \
+	-ldflags '-w' `# disables debug letting the file be smaller` \
+	-ldflags '-linkmode external' \
+	-ldflags '-extldflags "-static"' \
 	-ldflags "${ldflags}" \
 	-o /go/bin/${BASE}-Linux-x86_64 \
 	./${NODE} # otherwise -> go build: cannot use -o with multiple packages
 
 # - macos
-CGO_ENABLED=0 \
+# CGO_ENABLED=1 \
+# CC='o64-clang++' \
 GOOS=darwin GOARCH=amd64 go build \
-	-a -tags netgo \
+	`# -a # force rebuilding of packages that are already up-to-date` \
+	`# -tags netgo # make sure we use built-in net package and not the system’s one` \
 	-v \
-	-ldflags '-w -extldflags "-static"' \
+	-ldflags '-w' `# disables debug letting the file be smaller` \
+	-ldflags '-linkmode external' \
+	-ldflags '-extldflags "-static"' \
 	-ldflags "${ldflags}" \
 	-o /go/bin/${BASE}-Darwin-x86_64 \
 	./${NODE} # otherwise -> go build: cannot use -o with multiple packages
 
+# - raspberry pi
+# CC='arm-linux-musleabihf-gcc' \
+# CGO_ENABLED=1 \
+# CC='musl-gcc' \
+GOOS=linux GOARCH=arm GOARM=7 go build \
+	`# -a # force rebuilding of packages that are already up-to-date` \
+	`# -tags netgo # make sure we use built-in net package and not the system’s one` \
+	-v \
+	-ldflags '-w' `# disables debug letting the file be smaller` \
+	-ldflags '-linkmode external' \
+	-ldflags '-extldflags "-static"' \
+	-ldflags "${ldflags}" \
+	-o /go/bin/${BASE}-Linux-arm \
+	./${NODE} # otherwise -> go build: cannot use -o with multiple packages
+
 # chmod binaries to executable
 chmod +x /go/bin/${BASE}-Linux-x86_64
+chmod +x /go/bin/${BASE}-Linux-arm
 chmod +x /go/bin/${BASE}-Darwin-x86_64
