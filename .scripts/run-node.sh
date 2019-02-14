22c11a071926c0dfc4b7534727f272728c1e3858
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-02-14 20:00:19 +0300

    (arch): fix scripts for the running service as they's directory is the root

--- .scripts/run-node.sh
+++ .scripts/run-node.sh
@@ -4,23 +4,17 @@ set -eux
 # if first arg is node in monorepo graph - apply commands to this node (package),
 # otherwise - to root (all repo nodes)
 NODE=${1:-.}
+BASE=`basename "${NODE}"`
 
+### PHASE 1. Preparing
 # install external dependencies on docker image
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get update
 apt-get install -y nodejs
 npm install --production --prefix=${NODE}
-# apk add --update --no-cache \
-	# jq \
-	# git \
-	# imagemagick \
-	# python \
-	# curl \
-	# py-pip
-	# py-lxml
-	# chromium \
 
-# build base
+### PHASE 2. Building and compiling
+# build base for runnning as binary
 # fix, lint and build source code of base app
 .scripts/fmt.sh 'base'
 .scripts/build-bin.sh 'base'
@@ -30,15 +24,21 @@ npm install --production --prefix=${NODE}
 .scripts/fmt.sh ${NODE}
 .scripts/build.sh ${NODE}
 
-# copy necessary (bins, libs, etc) from /go/bin to our dir (conf needs it here because related path to node root)
-# TODO: resolve: cp /go/bin/${NODE}-$(uname -s)-$(uname -m) ./${NODE}
+# move necessary (bins, libs, etc) from /go/bin to our dir (conf needs it here because related path to node root)
+mv /go/bin/${BASE}-*-* ./${NODE}
+
+### PHASE 3. Running
+# NOTE: run under ${NODE} directory as root
+cd ${NODE}
 
 # run base with microservice cli onboard
 # set application variables for run base
 # TODO move to special config in future named boomfunc.yaml
 # TODO https://github.com/urfave/cli#values-from-alternate-input-sources-yaml-toml-and-others
 BMP_BASE_DEBUG_MODE=true \
-BMP_BASE_CONFIG="./${NODE}/router.yml" \
+BMP_BASE_CONFIG="./router.yml" \
 BMP_BASE_APP_LAYER='http' \
-BMP_BASE_WORKER_NUM=2 \
 	/go/bin/base-$(uname -s)-$(uname -m) run tcp
+
+# move back to monorepo root
+cd -
