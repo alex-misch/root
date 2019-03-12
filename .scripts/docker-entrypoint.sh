8650abb6aa4ee8ec024de639f725a6f3ed61bd80
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-03-12 17:45:15 +0300

    fix typo in comment

--- .scripts/docker-entrypoint.sh
+++ .scripts/docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -eux
 # main entrypoint for docker container microservice run
 NODE='bmpjs/ssr'
 
-# run script based on type on launch type
+# run script based on launch type
 # .scripts/run-go.sh ${NODE}
 .scripts/run-node.sh ${NODE}
