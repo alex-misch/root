57e6a295694822d58c7cecf2d6ec362fae231e3f
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-03-13 18:49:31 +0300

    fix bench

--- base/tools/exec_bench_test.go
+++ base/tools/exec_bench_test.go
@@ -1,7 +1,6 @@
 package tools
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -17,22 +16,7 @@ func BenchmarkCLISplit(b *testing.B) {
 	--opt8='HEAD / HTTP/1.1\r\n\r\n'
 	cmd  `
 
-	words := []string{
-		"bin",           // multiline (not trimmed)
-		"-opt1", "arg1", // simple
-		"--opt2=arg2",         // another simple
-		"--opt2.5", "foo bar", // escaped single space
-		"--opt3", "foo bar", // escaped group by quote (string) with space
-		"--opt4=foo bar",     // escaped group by quote (weak) with space
-		"-opt5", "foo's bar", // quote in quoted group (another)
-		"--opt7=foo= bar \"baz\"",            // quote in quoted group (another) (not trimmed)
-		"--opt8=HEAD / HTTP/1.1\\r\\n\\r\\n", // special symbols in argument (\r or \n or \t) (additional escaping - look at `special symbols` section below)
-		"cmd",                                // (not trimmed)
-	}
-
 	for i := 0; i < b.N; i++ {
-		if output := CLISplit(input); !reflect.DeepEqual(output, words) {
-			b.Fatalf("Expected %q, got %q", words, output)
-		}
+		CLISplit(input)
 	}
 }
