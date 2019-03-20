aa61eae3567db503eaeb1ee6426bdc2cf4359646
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-03-20 18:56:48 +0300

    fix script

--- .scripts/bench.sh
+++ .scripts/bench.sh
@@ -4,7 +4,6 @@ set -eux
 # if first arg is node in monorepo graph - apply commands to this node (package),
 # otherwise - to root (all repo nodes)
 NODE=${1:-.}
-NODE='base/pipeline'
 
 go get -d -t ./${NODE}/...
 go clean -testcache || true
