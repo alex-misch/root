1cc0ece5e608b246dbf72a317436c6146da28455
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-03-22 15:39:30 +0300

    fix plugin: now load library only on first invoke

--- base/pipeline/plugin.go
+++ base/pipeline/plugin.go
@@ -2,42 +2,44 @@ package pipeline
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"plugin"
 
 	"github.com/boomfunc/root/tools/flow"
 )
 
-type Plugin struct {
+var (
+	ErrPluginNoStep = errors.New("base/pipeline/plugin: no step loaded")
+)
+
+// plug is underlying struct describes raw data for loading go plugin as a `step`
+type plug struct {
 	path string
 	name string
-
+	step flow.Step
 	stdio
 }
 
-func NewPlugin(path, name string) *Plugin {
-	return &Plugin{path: path, name: name}
+// NewPlugin returns pipeline.Layer of plugin type
+func NewPlugin(path, name string) Layer {
+	return &plug{
+		path: path,
+		name: name,
+	}
 }
 
-func (p *Plugin) copy() Layer {
+func (p *plug) copy() Layer {
 	clone := *p
 	return &clone
 }
 
-func (p *Plugin) prepare(ctx context.Context) error {
-	return nil
-}
-
-func (p *Plugin) check(ctx context.Context) error {
-	// check layer piped
-	if err := p.checkStdio(); err != nil {
-		return err
+func (p *plug) prepare(ctx context.Context) error {
+	// step already loaded
+	if p.step != nil {
+		return nil
 	}
 
-	return nil
-}
-
-func (p *Plugin) run(ctx context.Context) error {
 	// 1. open the so file to load the symbols
 	pl, err := plugin.Open(p.path)
 	if err != nil {
@@ -56,15 +58,35 @@ func (p *Plugin) run(ctx context.Context) error {
 		return fmt.Errorf("base/pipeline/plugin: %s", err)
 	}
 
+	// 4. save step for future invoking
+	p.step = step
+
+	return nil
+}
+
+func (p *plug) check(ctx context.Context) error {
+	// check layer piped
+	if err := p.checkStdio(); err != nil {
+		return err
+	}
+
+	// check step already loaded
+	if p.step == nil {
+		return ErrPluginNoStep
+	}
+
+	return nil
+}
+
+func (p *plug) run(ctx context.Context) error {
 	// TODO: look not elegnt, i want `fake` plugin's stout and stdin and stderr
 	ctx = context.WithValue(ctx, "stdin", p.stdin)
 	ctx = context.WithValue(ctx, "stdout", p.stdout)
 	// ctx = context.WithValue(ctx, "srderr", p.srderr)
 
-	// 4. run
-	return step.Run(ctx)
+	return flow.ExecuteWithContext(ctx, p.step)
 }
 
-func (p *Plugin) close(ctx context.Context) error {
+func (p *plug) close(ctx context.Context) error {
 	return p.closeStdio()
 }
