b6fed3b52457975b1a46aee7f54d6ef9d2da4fb0
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-04-09 17:25:52 +0300

    fix dispatcher tools tests

--- tools/flow2/dispatcher_tools_test.go
+++ tools/flow2/dispatcher_tools_test.go
@@ -5,67 +5,54 @@ import (
 	"testing"
 )
 
+func check(t *testing.T, h heap.Interface, c, l int) {
+	// get raw channel type
+	ch := h.(workers)
+
+	// check capacity
+	if got := cap(ch); got != c {
+		t.Fatalf("cap: Expected %q, got %q", c, got)
+	}
+	// check lenght
+	if got := len(ch); got != l {
+		t.Fatalf("len: Expected %q, got %q", l, got)
+	}
+	if got := h.Len(); got != l {
+		t.Fatalf("Len: Expected %q, got %q", l, got)
+	}
+}
+
 func TestWorkersHeap(t *testing.T) {
 	var h heap.Interface
 
 	t.Run("New", func(t *testing.T) {
 		h = WorkersHeap(5)
-		ch := h.(workers)
-
-		// maximum available places for workers
-		if c := cap(ch); c != 5 {
-			t.Fatalf("Expected %q, got %q", 5, c)
-		}
-
-		// at creation stage heap must be full
-		if l := len(ch); l != 5 {
-			t.Fatalf("Expected %q, got %q", 5, l)
-		}
+		check(t, h, 5, 5) // at the beginning capacity and len are full
 	})
 
 	t.Run("Pop", func(t *testing.T) {
-		ch := h.(workers)
-
-		// Pop worker from heap
-		if worker := h.Pop(); worker != WORKER {
-			t.Fatalf("Expected %q, got %q", WORKER, worker)
+		// pop all workers!
+		for i := 0; i < 5; i++ {
+			// Pop worker from heap
+			if worker := h.Pop(); worker != WORKER {
+				t.Fatalf("Expected %q, got %q", WORKER, worker)
+			}
+			check(t, h, 5, 5-(i+1))
 		}
-		// maximum available places for workers
-		if c := cap(ch); c != 5 {
-			t.Fatalf("Expected %q, got %q", 5, c)
-		}
-		// at creation stage heap must be full
-		if l := len(ch); l != 4 {
-			t.Fatalf("Expected %q, got %q", 4, l)
-		}
+		check(t, h, 5, 0)
+		// next .Pop() will hung, not enough free workers
 	})
 
 	t.Run("Push", func(t *testing.T) {
-		ch := h.(workers)
-
-		// Push worker to heap
-		h.Push(nil)
-		// maximum available places for workers
-		if c := cap(ch); c != 5 {
-			t.Fatalf("Expected %q, got %q", 5, c)
+		// after all .Pop - empty dispatcher
+		check(t, h, 5, 0)
+		// return all workers to heap
+		for i := 0; i < 5; i++ {
+			// Pop worker from heap
+			h.Push(nil)
+			check(t, h, 5, i+1)
 		}
-		// at creation stage heap must be full
-		if l := len(ch); l != 5 {
-			t.Fatalf("Expected %q, got %q", 5, l)
-		}
-	})
-
-	t.Run("Len", func(t *testing.T) {
-		if l := h.Len(); l != 5 {
-			t.Fatalf("Expected %q, got %q", 5, l)
-		}
-		h.Pop()
-		if l := h.Len(); l != 4 {
-			t.Fatalf("Expected %q, got %q", 4, l)
-		}
-		h.Push(nil)
-		if l := h.Len(); l != 5 {
-			t.Fatalf("Expected %q, got %q", 5, l)
-		}
+		check(t, h, 5, 5)
+		// next .Push() will hung, because not enough free space in dispatcher
 	})
 }
