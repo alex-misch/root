252e07667953fe3f5c4802bd5e211b6d0f2532e5
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-04-09 19:34:39 +0300

    add transaction proxy step

---
+++ tools/flow2/proxy_transaction.go
@@ -0,0 +1,61 @@
+package flow2
+
+import (
+	"context"
+	"fmt"
+)
+
+// transaction is Step with rollback functionality in case of error
+type transaction struct {
+	up    Step // step to achieve the desired state
+	down  Step // step to rollback to the initial state
+	force bool // indicates that initial state must be rolled back anyway (even if no errors in up)
+}
+
+// Transaction returns new `Step` of transaction type
+func Transaction(up, down Step, force bool) Step {
+	// special case - if not up - there is nothing to do - no need to create transaction
+	if up == nil {
+		return nil
+	}
+
+	// special case - if up and not down - there is no to rollback - no need to create transaction
+	if down == nil {
+		return up
+	}
+
+	// there is something to up and something to rollback, full transaction
+	return &transaction{up, down, force}
+}
+
+// backward is small tool for rolling back
+func (t *transaction) backward(ctx context.Context) error {
+	if t.down != nil {
+		return t.down.Run(ctx)
+	}
+
+	return nil
+}
+
+// Run implements Step interface
+func (t *transaction) Run(ctx context.Context) error {
+	// check on orphan
+	if t.up == nil {
+		return ErrStepOrphan
+	}
+
+	// forward stage
+	if err := t.up.Run(ctx); err != nil {
+		t.backward(ctx) // forward movement failed, need to rollback
+		return err      // but return error from гз
+	} else if t.force {
+		return t.backward(ctx) // forward movement failed, need to rollback and return error from backward stage
+	}
+
+	return nil
+}
+
+// String implements fmt.Stringer interface
+func (t *transaction) String() string {
+	return fmt.Sprintf("TRANSACTION(\n%s\n%s\n)", t.up, t.down)
+}
---
+++ tools/flow2/proxy_transaction_test.go
@@ -0,0 +1,93 @@
+package flow2
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestTransaction(t *testing.T) {
+	t.Run("New", func(t *testing.T) {
+		// pseudo steps
+		var up Step = dummy(1)
+		var down Step = dummy(1)
+
+		tableTests := []struct {
+			up    Step
+			down  Step
+			final Step
+		}{
+			{nil, nil, nil},
+			{nil, down, nil},
+			{up, nil, up},
+			{up, down, Transaction(up, down, false)},
+		}
+		for i, tt := range tableTests {
+			t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+				if final := Transaction(tt.up, tt.down, false); !reflect.DeepEqual(final, tt.final) {
+					t.Fatalf("expected %q, got %q", tt.final, final)
+				}
+			})
+		}
+	})
+
+	t.Run("Run", func(t *testing.T) {
+		var a, b int
+		var errUp = errors.New("Up error")
+		var errDown = errors.New("Down error")
+		// // pseudo steps
+		var upSuccess Step = Func(func(ctx context.Context) error {
+			a++
+			return nil
+		})
+		var upError Step = Func(func(ctx context.Context) error {
+			b++
+			return errUp
+		})
+		var downSuccess Step = Func(func(ctx context.Context) error {
+			a++
+			return nil
+		})
+		var downError Step = Func(func(ctx context.Context) error {
+			b++
+			return errDown
+		})
+
+		tableTests := []struct {
+			up    Step
+			down  Step
+			force bool
+			err   error
+		}{
+			{nil, nil, false, ErrStepOrphan}, // up == nil (orphan)
+
+			{upSuccess, nil, false, nil},          // up success (no force)
+			{upSuccess, nil, true, nil},           // up success (force)
+			{upSuccess, downSuccess, true, nil},   // up success (force with success)
+			{upSuccess, downSuccess, false, nil},  // up success (no force with success)
+			{upSuccess, downError, false, nil},    // up success (no force with error)
+			{upSuccess, downError, true, errDown}, // up success (force with error)
+
+			{upError, nil, false, errUp},         // up error, no down (no force)
+			{upError, nil, true, errUp},          // up error, no down (force)
+			{upError, downSuccess, false, errUp}, // up error, down success (no force)
+			{upError, downError, false, errUp},   // up error, down error (no force)
+
+			{upError, downSuccess, true, errUp}, // up error, down success (force)
+			{upError, downError, true, errUp},   // up error, down error (force)
+		}
+		for i, tt := range tableTests {
+			t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+				// reset counters
+				a, b = 0, 0
+
+				step := &transaction{tt.up, tt.down, tt.force}
+				if err := step.Run(context.TODO()); err != tt.err {
+					t.Fatalf("err: expected %q, got %q", tt.err, err)
+				}
+			})
+		}
+	})
+}
