92a928f24daf38bcdcf20dc4e5967414cfc2763b
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-04-22 19:45:31 +0300

    fix tests

--- guard/trust/main_test.go
+++ guard/trust/main_test.go
@@ -41,24 +41,36 @@ func TestCheck(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	remarker, err := Create(node(2), node(1))
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	tableTests := []struct {
 		// input
-		from Node
-		to   Node
+		marker []byte
+		from   Node
+		to     Node
 		// output
 		condition bool
 	}{
-		{nil, nil, false},
-		{node(1), nil, false},
-		{nil, node(2), false},
-		{node(1), node(2), true},  // wright direction, existing relation
-		{node(2), node(1), false}, // relation are not vise versa
+		{nil, nil, nil, false},
+		{nil, node(1), nil, false},
+		{nil, nil, node(2), false},
+		{[]byte("wrong"), node(1), nil, false},     // wrong marker
+		{[]byte("wrong"), nil, node(2), false},     // wrong marker
+		{[]byte("wrong"), node(1), node(2), false}, // wrong marker
+
+		{remarker, node(1), node(2), false}, // wrong marker, existing relation
+		{marker, node(1), node(2), true},    // wright direction, existing relation
+
+		{marker, node(2), node(1), false},  // relation are not vise versa
+		{remarker, node(2), node(1), true}, // relation are not vise versa
 	}
 
 	for i, tt := range tableTests {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			if condition := Check(marker, tt.from, tt.to); condition != tt.condition {
+			if condition := Check(tt.marker, tt.from, tt.to); condition != tt.condition {
 				t.Fatalf("Expected \"%t\", got \"%t\"", tt.condition, condition)
 			}
 		})
