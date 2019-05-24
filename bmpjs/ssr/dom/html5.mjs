d6479f511f9c0037f45a763854d969fa3a8ffd8d
Alexander Mishchenko <h-alex@bk.ru>
2019-05-24 21:39:15 +0300

    fix(log): rm trash

--- bmpjs/ssr/dom/html5.mjs
+++ bmpjs/ssr/dom/html5.mjs
@@ -67,7 +67,6 @@ const HTML5Api = ({ url, userAgent, baseURI }) => ({
 	customElements,
 	CustomEvent: class {},
 	fetch: async (...args) => {
-		console.error(...args)
 		const fetchTime = (new Date()).getTime()
 		const res = await fetch(...args)
 		console.warn('Fetch end in', timeStamp(fetchTime), args[0])
