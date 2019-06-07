c71849128276d722dd292b8e04ae30bac3e04319
Alexander Gurinov <alexander.gurinov@gmail.com>
2019-06-07 20:05:15 +0300

    add graphql step generator

---
+++ base/server/mux/graphql.go
@@ -0,0 +1,57 @@
+package mux
+
+// Set of tools for quick graphql implementation.
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/boomfunc/root/tools/flow"
+	"github.com/graphql-go/graphql"
+)
+
+// GraphQL return step generated with provided schema.
+func GraphQL(schema graphql.Schema) flow.Step {
+	return flow.Func(func(ctx context.Context) error {
+		// TODO: deprecated phase
+		stdin, ok := ctx.Value("stdin").(io.Reader)
+		if !ok {
+			return flow.ErrStepOrphan
+		}
+		stdout, ok := ctx.Value("stdout").(io.Writer)
+		if !ok {
+			return flow.ErrStepOrphan
+		}
+		// TODO: end of deprecated phase
+
+		w, ok := ctx.Value("w").(http.ResponseWriter)
+		if ok {
+			// If we have HTTP mode - set headers and status code.
+			w.Header().Set("Content-Type", "application/json")
+		}
+
+		// Phase 1. Fetch graphql query.
+		var query string
+
+		// Also, in http mode, a query can be in get parameters or post body.
+		if r := ctx.Value("r").(*http.Request); ok && r.Method == "GET" {
+			query = r.URL.Query().Get("query")
+		} else {
+			// In other cases (`no http` or `post request`) the query is the stdin itself.
+			var b bytes.Buffer
+			b.ReadFrom(stdin)
+			query = b.String()
+		}
+
+		// Phase 2. Run graphql and return results.
+		result := graphql.Do(graphql.Params{
+			Schema:        schema,
+			RequestString: query,
+		})
+		return json.NewEncoder(stdout).Encode(result)
+	})
+}
